8dcfdfa57e82b4eac8cb00bec98a10c49d53b66d
dxize <159326372+dxize@users.noreply.github.com>
2026-04-24 23:36:25 +0300

    test(codegen): update unit tests for compiled program generation

--- tests/VirtualMachineCodegen.UnitTests/DeaVmCodegenTests.cs
+++ tests/VirtualMachineCodegen.UnitTests/DeaVmCodegenTests.cs
@@ -12,7 +12,7 @@ namespace VirtualMachineCodegen.UnitTests;
 public class DeaVmCodegenTests
 {
     [Fact]
-    public void Generates_push_and_print_for_each_literal_and_halt_for_return()
+    public void Generates_push_and_print_for_each_literal_and_return_for_main_program()
     {
         string code = """
             func int main() {
@@ -24,25 +24,34 @@ public class DeaVmCodegenTests
         Ast.ProgramNode program = new DeaParser(code).ParseProgram();
         DeaVmCodegen codegen = new();
 
-        IReadOnlyList<Instruction> instructions = codegen.Generate(program);
+        IReadOnlyList<Instruction> instructions = codegen.GenerateProgram(program).Instructions;
 
-        Assert.Equal(8, instructions.Count);
-        Assert.Equal(InstructionCode.Push, instructions[0].Code);
-        Assert.Equal(1, instructions[0].Operand.AsInt());
-        Assert.Equal(InstructionCode.CallBuiltin, instructions[1].Code);
-        Assert.Equal((int)BuiltinFunctionCode.Print, instructions[1].Operand.AsInt());
+        Assert.Equal(11, instructions.Count);
 
-        Assert.Equal(InstructionCode.Push, instructions[2].Code);
-        Assert.Equal(2.5, instructions[2].Operand.AsNum());
-        Assert.Equal(InstructionCode.CallBuiltin, instructions[3].Code);
+        Assert.Equal(InstructionCode.Jump, instructions[0].Code);
 
-        Assert.Equal(InstructionCode.Push, instructions[4].Code);
-        Assert.Equal("dea", instructions[4].Operand.AsString());
-        Assert.Equal(InstructionCode.CallBuiltin, instructions[5].Code);
+        Assert.Equal(InstructionCode.Push, instructions[1].Code);
+        Assert.Equal(1, instructions[1].Operand.AsInt());
+        Assert.Equal(InstructionCode.CallBuiltin, instructions[2].Code);
+        Assert.Equal((int)BuiltinFunctionCode.Print, instructions[2].Operand.AsInt());
 
-        Assert.Equal(InstructionCode.Push, instructions[6].Code);
-        Assert.Equal(7, instructions[6].Operand.AsInt());
-        Assert.Equal(InstructionCode.Halt, instructions[7].Code);
+        Assert.Equal(InstructionCode.Push, instructions[3].Code);
+        Assert.Equal(2.5, instructions[3].Operand.AsNum());
+        Assert.Equal(InstructionCode.CallBuiltin, instructions[4].Code);
+        Assert.Equal((int)BuiltinFunctionCode.Print, instructions[4].Operand.AsInt());
+
+        Assert.Equal(InstructionCode.Push, instructions[5].Code);
+        Assert.Equal("dea", instructions[5].Operand.AsString());
+        Assert.Equal(InstructionCode.CallBuiltin, instructions[6].Code);
+        Assert.Equal((int)BuiltinFunctionCode.Print, instructions[6].Operand.AsInt());
+
+        Assert.Equal(InstructionCode.Push, instructions[7].Code);
+        Assert.Equal(7, instructions[7].Operand.AsInt());
+        Assert.Equal(InstructionCode.Return, instructions[8].Code);
+
+        Assert.Equal(InstructionCode.CallUser, instructions[9].Code);
+        Assert.Equal("main", instructions[9].Operand.AsString());
+        Assert.Equal(InstructionCode.Halt, instructions[10].Code);
     }
 
     [Fact]
@@ -59,7 +68,7 @@ public class DeaVmCodegenTests
         Ast.ProgramNode program = new DeaParser(code).ParseProgram();
         DeaVmCodegen codegen = new();
 
-        IReadOnlyList<Instruction> instructions = codegen.Generate(program);
+        IReadOnlyList<Instruction> instructions = codegen.GenerateProgram(program).Instructions;
 
         Assert.NotEmpty(instructions);
     }
@@ -69,23 +78,19 @@ public class DeaVmCodegenTests
     {
         DeaVmCodegen codegen = new();
 
-        // Func без return внутри тела (ошибка семантики, но валидно для AST)
         FunctionDeclaration funcWithoutReturn = new(
             DataType.Int,
             "noReturnFunc",
             new List<ParameterDeclaration>(),
-            new List<AstNode>()
-        );
+            new List<AstNode>());
 
         ProgramNode program = new(
             new List<Declaration>(),
             new List<FunctionDeclaration> { funcWithoutReturn },
-            new FunctionDeclaration(DataType.Int, "main", new List<ParameterDeclaration>(), new List<AstNode>())
-        );
+            new FunctionDeclaration(DataType.Int, "main", new List<ParameterDeclaration>(), new List<AstNode>()));
 
         CompiledProgram compiled = codegen.GenerateProgram(program);
 
-        // Проверка что добавился Value.Void при отсутствии return
         Assert.Contains(compiled.Instructions, i => i.Code == InstructionCode.Push && i.Operand.IsVoid());
     }
 }
